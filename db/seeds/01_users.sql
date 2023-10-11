08479ab40bb988b9dd70f52c0c359ee9aab52169
jjyh <janejyh@gmail.com>
2023-10-11 17:19:42 -0400

    added seed data to db tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Luke', 'luke@sw.com', 'skywalker');
+INSERT INTO users (name, email, password) VALUES ('Hans', 'hans@sw.com', 'solo');
+INSERT INTO users (name, email, password) VALUES ('Leia', 'leia@sw.com', 'organa');
+INSERT INTO users (name, email, password) VALUES ('Obiwan', 'obiwan@sw.com', 'kenobi');
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,5 @@
+-- Users table seeds here (Example)
+INSERT INTO categories (name) values ('Films');
+INSERT INTO categories (name) values ('Restaurants');
+INSERT INTO categories (name) values ('Books');
+INSERT INTO categories (name) values ('Products');
