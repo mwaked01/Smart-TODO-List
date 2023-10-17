56e8b2119c42da12887c236ce0d76ca0c7a5b7ac
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-10-17 12:39:11 -0400

    Redirect to homepage; remove login func

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,5 +1,13 @@
-//route for homepage: "/"
+//route for logged in user: "/"
 
-app.get("/", (req, res)) => {
-  
-}
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/login', (req, res) => {
+  res.send("Welcome");
+});
+
+app.post('/login', (req, res) => {
+  res.redirect('/home');
+});
